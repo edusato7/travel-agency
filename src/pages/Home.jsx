aa4127b88ed1eae85e18eafb44540cefde162b71
Eduardo Sato <eduardosatodias@hotmail.com>
2025-02-25 10:27:20 -0300

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className='absolute inset-0 bg-black/50 flex flex-col items-center justify-center'>
         <h1 className='text-4xl md:text-6xl font-bold text-white mb-4 text-center px-10'>Explore o Mundo conosco!</h1>
         <p className='text lg md:text-2xl text-white mb-8'>Descubra lugares incríveis com ofertas exclusivas.</p>
-        <a href="/"><button
+        <a href=""><button
         className='border font-bold text-white px-6 py-2 rounded-full text-lg md:text-xl hover:bg-blue-500
             transform transition duration-300 hover:scale-105 cursor-pointer'
         >Explorar</button></a>
